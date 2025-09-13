008bd73c6779ab7c1a799d03006364afd274f121
2saly2 <aitlahssainenouhaila@gmail.com>
2025-09-13 15:28:21 +0100

    exercice_3

---
+++ week_1/day_1/exercices/Ex_3.py
@@ -0,0 +1,7 @@
+your_name = input("enter your name (:")
+your_name = str(your_name)
+my_name ="nouhaila"
+if my_name == your_name :
+    print("Same name? Guess we're best friends now (;")
+else :
+    print("nice name, but mine is cooler (;")   
