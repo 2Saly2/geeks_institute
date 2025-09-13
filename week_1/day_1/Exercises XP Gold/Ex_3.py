04684974c17db9b01aa0e2a98c601578a88b0b7d
2saly2 <aitlahssainenouhaila@gmail.com>
2025-09-13 23:06:36 +0100

    exercice_3

---
+++ week_1/day_1/Exercises XP Gold/Ex_3.py
@@ -0,0 +1,7 @@
+my_name = "nouhaila"
+your_name = input("Write your name ")
+while my_name != your_name :
+        your_name = input("Write your name ")
+         
+print(" we have the same name")
+      
