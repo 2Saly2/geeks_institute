8522b3510327b72655f2746279ce264cade2d478
2saly2 <aitlahssainenouhaila@gmail.com>
2025-09-16 07:58:39 +0100

    exercice_6

---
+++ week_1/day_2/Exercises XP/Ex_6.py
@@ -0,0 +1,7 @@
+def make_shirt(size="Large", text="I love Python"):
+    print(f"The size of the shirt is {size} and the text is {text}")
+
+make_shirt()
+make_shirt("Medum")
+make_shirt("smull", "How are you?") 
+make_shirt(text="python" , size="XXL")
