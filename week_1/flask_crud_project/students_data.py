8a5bdf24badefac3c54eacaaf089ca2de45df218
2saly2 <aitlahssainenouhaila@gmail.com>
2025-09-22 14:18:09 +0100

    remove flask_crud_project from repo

--- week_1/flask_crud_project/students_data.py
+++
@@ -1,7 +0,0 @@
-students = [
-    {"id": 1, "name": "John Doe", "email": "john.doe@example.com", "age": 20, "gender": "male"},
-    {"id": 2, "name": "Jane Doe", "email": "jane.doe@example.com", "age": 21, "gender": "female"},
-    {"id": 3, "name": "Jim Doe", "email": "jim.doe@example.com", "age": 22, "gender": "male"},
-    {"id": 4, "name": "Jill Doe", "email": "jill.doe@example.com", "age": 23, "gender": "female"},
-    {"id": 5, "name": "Jack Doe", "email": "jack.doe@example.com", "age": 24, "gender": "male"},
-]
