fdb452d857f1f9fe4d189bbdbd0e096208f7d176
2saly2 <aitlahssainenouhaila@gmail.com>
2025-09-24 13:54:51 +0100

    exercices 1/2

---
+++ week_2/day_2/Exercises XP/Ex_1.sql
@@ -0,0 +1,13 @@
+SELECT * FROM items
+ORDER BY price ASC;
+
+SELECT * FROM items
+WHERE price >=80
+ORDER BY price DESC;
+
+SELECT first_name,last_name FROM custumers
+ORDER BY firsyt_name ASC
+LIMIT 3;
+
+SELECT last_name FROM custumers
+ORDER BY last_name DESC;
