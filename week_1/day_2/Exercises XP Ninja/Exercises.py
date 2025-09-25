ff467b02f0e747b42c85bbfb4ba4c944bb813e75
2saly2 <aitlahssainenouhaila@gmail.com>
2025-09-25 21:43:56 +0100

    remove Ex_2.py  Add Exercises.py

--- week_1/day_2/Exercises XP Ninja/Exercises.py
+++ week_1/day_2/Exercises XP Ninja/Exercises.py
@@ -1,3 +1,4 @@
+# Exercise 1 :
 s="Volkswagen,Toyota,Ford Motor,Honda,Chevrolet"
 c=["Honda", "Volkswagen", "Toyota", "Ford Motor", "Honda", "Chevrolet", "Toyota"]
 cars=s.split(",")
@@ -29,3 +30,20 @@ set_cars =list(set(c))
 result = ", ".join(set_cars)
 print(set_cars)
 print(result)
+
+# Exercise 2 :
+
+def get_full_name(first_name, last_name,middle_name=None):
+
+    first_name=first_name.strip().capitalize()
+    last_name=last_name.strip().capitalize()
+
+    if middle_name :
+        middle_name=middle_name.strip().capitalize()
+        full_name= f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name=f"{first_name} {last_name}"
+    return full_name
+
+print(get_full_name("mohamed","ali","ahmed"))
+print(get_full_name("mohamed","ali"))
