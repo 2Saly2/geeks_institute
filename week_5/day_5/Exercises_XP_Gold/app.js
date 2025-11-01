16652842fad5f4564acd0e0d25bd6316023b12d1
2saly2 <aitlahssainenouhaila@gmail.com>
2025-11-01 14:35:49 +0100

    app

--- week_5/day_5/Exercises_XP_Gold/app.js
+++ week_5/day_5/Exercises_XP_Gold/app.js
@@ -113,7 +113,7 @@ app.get('/api/profile', (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+app.listen(5000, () => console.log('Server running on http://localhost:5000')); 
 
 // ***************************************************************
 
